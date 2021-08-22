ea6017180b17ffb7a92f3143380ca49796eb3d7c
DimaEff <fominienkovd@yandex.ru>
2021-08-23 01:30:40 +0300

    sdvszd bDB ZD B

--- build/static/js/6.2ab82dde.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpinkerri-houses"]=this["webpackJsonpinkerri-houses"]||[]).push([[6],{410:function(s,e,i){"use strict";i.r(e);i(0);var n=i(1);e.default=function(){return Object(n.jsx)("div",{children:"Photos"})}}}]);
-//# sourceMappingURL=6.2ab82dde.chunk.js.map
